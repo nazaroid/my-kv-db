4454af97188ecc5349290855d2a16ec8d07f49e3
artem.nazarenko <a.nazarenko@UZUM.COM>
2025-05-03 00:57:33 +0300

    • kvdb init in progress

--- modules/server/src/it/scala/org/nazaroid/kvdb/TestDbServerFactory.scala
+++ modules/server/src/it/scala/org/nazaroid/kvdb/TestDbServerFactory.scala
@@ -11,12 +11,12 @@ import org.typelevel.log4cats.slf4j.Slf4jLogger
 import scala.concurrent.ExecutionContext
 
 final class TestDbServerFactory {
-  private implicit val runtime: IORuntime = IORuntime.builder().build()
-  private implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
-  private implicit val logger: Logger[IO] = Slf4jLogger.getLogger[IO]
-  private val di = new DiContainer[IO]
 
   def create(appConfig: AppConfig): IO[DbServer[IO]] = {
+    implicit val runtime: IORuntime = IORuntime.builder().build()
+    implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+    implicit val logger: Logger[IO] = Slf4jLogger.getLogger[IO]
+    val di = new DiContainer[IO]
     Dispatcher.parallel[IO] use { implicit d: Dispatcher[IO] =>
       di.resolveDbServer(appConfig)
     }
