ed8247a5746ef4528cc11fe2159d5705ae31f18e
artem.nazarenko <a.nazarenko@UZUM.COM>
2025-06-28 00:03:37 +0300

    • kvdb init in progress

--- modules/server/src/main/scala/org/nazaroid/kvdb/engine/bitcask/BitcaskDbEngine.scala
+++ modules/server/src/main/scala/org/nazaroid/kvdb/engine/bitcask/BitcaskDbEngine.scala
@@ -1,18 +1,13 @@
 package org.nazaroid.kvdb.engine.bitcask
 
-import cats.data.Kleisli
-import cats.data.Kleisli.ask
-import cats.effect.{Async, Ref}
+import cats.effect.Async
 import cats.implicits.*
 import org.nazaroid.kvdb.BitcaskConf
 import org.nazaroid.kvdb.algebra.{Database, DbEngine, Table}
 import org.nazaroid.kvdb.engine.bitcask.BitcaskDbEngine.*
-import org.nazaroid.kvdb.engine.bitcask.BitcaskDbEngine.algebra.*
-
-import java.nio.file.{Files, Path, Paths}
-// TODO: переделать дизайн, чтобы методы были отделены от данных
 
 final class BitcaskDbEngine[F[_]: Async](conf: BitcaskConf) extends DbEngine[F] {
+  private given lib: BitcaskLib.Scenarios[F] = BitcaskLib(conf)
 
   override def createDbIfNotExists(name: String): F[Database[F]] = {
     /* TODO:
@@ -21,19 +16,14 @@ final class BitcaskDbEngine[F[_]: Async](conf: BitcaskConf) extends DbEngine[F] 
      * при createDbIfNotExists:
        - создать папку для  бд
      */
-    val dbPath = Paths.get(f"${conf.rootDir}/$name")
-    val db = BitcaskDatabase[F](conf)(dbPath)
-    db.init().map(_ => db)
+    lib.createBaseIfNotExists(name).map(BitcaskDatabase[F](_))
   }
 }
 
 object BitcaskDbEngine {
 
-  private class BitcaskDatabase[F[_]: Async](conf: BitcaskConf)(dbPath: Path) extends Database[F] {
-
-    def init(): F[Unit] = {
-      Async[F].blocking(Files.createDirectories(dbPath))
-    }
+  private class BitcaskDatabase[F[_]: Async](base: BitcaskLib.algebra.Base)(using lib: BitcaskLib.Scenarios[F])
+      extends Database[F] {
 
     override def createTableIfNotExists(name: String): F[Table[F]] = {
       // TODO:
@@ -45,154 +35,17 @@ object BitcaskDbEngine {
       // для существующей:
       // - ничего не делать, вернуть объект
 
-      val tblPath = Paths.get(f"${dbPath.toAbsolutePath}/$name")
-      val tbl = BitcaskTable[F](conf)(tblPath)
-      tbl.init().map(_ => tbl)
+      lib.createTableIfNotExists(base, name).map(BitcaskTable[F](_))
     }
   }
 
-  private class BitcaskTable[F[_]: Async](conf: BitcaskConf)(tblPath: Path) extends Table[F] {
-
-    def init(): F[Unit] = {
-      Async[F].blocking(Files.createDirectories(tblPath))
-
-    }
+  private class BitcaskTable[F[_]: Async](tbl: BitcaskLib.algebra.Tbl)(using lib: BitcaskLib.Scenarios[F]) extends Table[F] {
 
     override def get(key: String): F[String] = ???
 
     override def set(key: String, value: String): F[Unit] = {
       ???
     }
-  }
-
-  object algebra {
-    type Offset = Long
-    type Key = String
-    type Value = String
-    type BaseName = String
-    type TblName = String
-    type TblIxName = String
-    type SegmentNum = Int
-    type SegmentIxData = Map[Key, Offset]
-    type TblIxData = Map[Key, Segment]
-    type BaseRegistry = Map[BaseName, Base]
-    type BaseTables = Map[TblName, Tbl]
-    type DbScript[F[_], O] = Kleisli[F, Env[F], O]
-
-    trait Env[F[_]] {
-      def conf: BitcaskConf
-
-      def files: FileService[F]
-
-      def base: BaseService[F]
-
-      def tbl: TblService[F]
-
-      def segment: TblSegmentService[F]
-
-      def tblIx: TblIxService[F]
-
-      def segmentIx: SegmentIxService[F]
-
-      def cache: CacheService[F]
-
-      def state: State[F]
-    }
-
-    trait CacheService[F[_]] {
-
-      def findSegment(
-        baseName: BaseName,
-        tblName: TblName,
-        k: Key
-      ):                                             DbScript[F, Option[Segment]]
-      def getOffsetInSegment(sx: SegmentIx, k: Key): DbScript[F, Offset]
-
-      def addBase(base: Base):          DbScript[F, BaseRegistry]
-      def addTbl(base: Base, tbl: Tbl): DbScript[F, Base]
-
-      def addSegment(
-        env: Env[F]
-      )(
-        base: Base,
-        tbl: Tbl,
-        s: Segment
-      ): DbScript[F, Base]
-    }
-
-    trait FileService[F[_]] {}
-
-    trait BaseService[F[_]] {
-      def createIfNotExists(rootDir: String, name: BaseName): DbScript[F, Base]
-      def list(rootDir: String):                              DbScript[F, Seq[Base]]
-    }
-
-    trait TblService[F[_]] {
-      def createIfNotExists(base: Base, name: TblName): DbScript[F, TblIx]
-      def list(base: Base):                             DbScript[F, Seq[TblIx]]
-    }
-
-    trait TblSegmentService[F[_]] {
-      def create(tbl: Tbl, num: SegmentNum):            DbScript[F, Segment]
-      def append(s: Segment, batch: Seq[(Key, Value)]): DbScript[F, Segment]
-      def readValue(s: Segment, offset: Offset):        DbScript[F, Value]
-    }
-
-    trait TblIxService[F[_]] {
-      def create(tbl: Tbl): DbScript[F, TblIx]
-      def read(tbl: Tbl):   DbScript[F, TblIx]
-      def write(ix: TblIx): DbScript[F, TblIx]
-    }
-
-    trait SegmentIxService[F[_]] {
-      def create(s: Segment):              DbScript[F, SegmentIx]
-      def read(tbl: Tbl, num: SegmentNum): DbScript[F, SegmentIx]
-      def write(ix: SegmentIx):            DbScript[F, SegmentIx]
-    }
-
-    final case class Base(
-      name:   BaseName,
-      path:   Path,
-      tables: BaseTables)
-
-    final case class TblIx(
-      name: TblIxName,
-      path: Path,
-      data: TblIxData)
-
-    final case class SegmentIx(
-      name: String,
-      path: Path,
-      data: SegmentIxData)
-
-    final case class Tbl(
-      name:        TblName,
-      path:        Path,
-      lastSegment: Segment,
-      ix:          TblIx)
-
-    final case class Segment(
-      num:        SegmentNum,
-      name:       String,
-      path:       Path,
-      isReadOnly: Boolean,
-      offset:     Offset,
-      ix:         SegmentIx)
-
-    final case class State[F[_]](registryRef: Ref[F, BaseRegistry])
-
-  }
-
-}
-
-trait BitcaskScenarios[F[_]: Async] {
-
-  def createBaseIfNotExists(baseName: String): DbScript[F, BaseRegistry] = {
-      for {
-        env <- ask[F, Env[F]]
-        base <- env.base.createIfNotExists(env.conf.rootDir, baseName)
-        registry <- env.cache.addBase(base)
-      } yield registry
   }
 
 }
---
+++ modules/server/src/main/scala/org/nazaroid/kvdb/engine/bitcask/BitcaskLib.scala
@@ -0,0 +1,234 @@
+package org.nazaroid.kvdb.engine.bitcask
+
+import cats.data.Kleisli
+import cats.data.Kleisli.ask
+import cats.effect.{Async, Ref}
+import cats.implicits.given
+import org.nazaroid.kvdb.BitcaskConf
+
+import java.nio.file.{Files, Path, Paths}
+
+object BitcaskLib {
+
+  import algebra.*
+  import instances.*
+
+  def apply[F[_]: Async](c: BitcaskConf): Scenarios[F] = new ScenariosImpl(c)
+
+  trait Scenarios[F[_]: Async] {
+    given env: Env[F]
+
+    def createBaseIfNotExists(baseName: String): F[Base] =
+      RunDbScript {
+        for {
+          env  <- ask[F, Env[F]]
+          base <- env.base.createIfNotExists(env.conf.rootDir, baseName)
+          _    <- env.cache.addBase(base)
+        } yield base
+      }
+
+    def createTableIfNotExists(base: Base, tblName: String): F[Tbl] =
+      RunDbScript {
+        for {
+          env <- ask[F, Env[F]]
+          tbl <- env.tbl.createIfNotExists(base, tblName)
+          _   <- env.cache.addTbl(base, tbl)
+        } yield tbl
+      }
+  }
+
+  object instances {
+
+    final class Dsl[F[_]: Async]
+        extends FileService[F]
+        with CacheService[F]
+        with BaseService[F]
+        with TblService[F]
+        with TblSegmentService[F]
+        with TblIxService[F]
+        with SegmentIxService[F] {}
+
+    final class ScenariosImpl[F[_]: Async](c: BitcaskConf) extends Scenarios[F]:
+      override def env: Env[F] = EnvImpl(c)
+
+    final class EnvImpl[F[_]: Async](c: BitcaskConf) extends Env[F]:
+      private val dsl = new Dsl()
+
+      override def conf: BitcaskConf = c
+
+      override def files: FileService[F] = dsl
+
+      override def base: BaseService[F] = dsl
+
+      override def tbl: TblService[F] = dsl
+
+      override def segment: TblSegmentService[F] = dsl
+
+      override def tblIx: TblIxService[F] = dsl
+
+      override def segmentIx: SegmentIxService[F] = dsl
+
+      override def cache: CacheService[F] = dsl
+
+      override def state: F[State[F]] =
+        Async[F].ref(Map.empty[BaseName, Base]) >>= { r =>
+          State(r).pure[F]
+        }
+  }
+
+  object algebra {
+    type Offset = Long
+    type Key = String
+    type Value = String
+    type BaseName = String
+    type TblName = String
+    type TblIxName = String
+    type SegmentNum = Int
+    type SegmentIxData = Map[Key, Offset]
+    type TblIxData = Map[Key, Segment]
+    type BaseRegistry = Map[BaseName, Base]
+    type BaseTables = Map[TblName, Tbl]
+    type DbScript[F[_], O] = Kleisli[F, Env[F], O]
+
+    trait Env[F[_]: Async] {
+      def conf: BitcaskConf
+
+      def files: FileService[F]
+
+      def base: BaseService[F]
+
+      def tbl: TblService[F]
+
+      def segment: TblSegmentService[F]
+
+      def tblIx: TblIxService[F]
+
+      def segmentIx: SegmentIxService[F]
+
+      def cache: CacheService[F]
+
+      def state: F[State[F]]
+    }
+
+    trait CacheService[F[_]] {
+
+      def findSegment(
+        baseName: BaseName,
+        tblName:  TblName,
+        k:        Key
+      ): DbScript[F, Option[Segment]] = ???
+
+      def getOffsetInSegment(sx: SegmentIx, k: Key): DbScript[F, Offset] = ???
+
+      def addBase(base: Base): DbScript[F, BaseRegistry] = ???
+
+      def addTbl(base: Base, tbl: Tbl): DbScript[F, Base] = ???
+
+      def addSegment(
+        env: Env[F]
+      )(
+        base: Base,
+        tbl:  Tbl,
+        s:    Segment
+      ): DbScript[F, Base] = ???
+    }
+
+    trait FileService[F[_]: Async] {
+
+      def createDirIfNotExists(path: Path): DbScript[F, Path] = {
+        Kleisli { env => Async[F].blocking(Files.createDirectories(path)) }
+      }
+    }
+
+    trait BaseService[F[_]: Async] {
+
+      def createIfNotExists(rootDir: String, name: BaseName): DbScript[F, Base] = {
+        for {
+          env <- ask[F, Env[F]]
+          dbPath = Paths.get(f"${env.conf.rootDir}/$name")
+          base = Base(name, dbPath)
+          _ <- env.files.createDirIfNotExists(base.path)
+        } yield base
+      }
+
+      def list(rootDir: String): DbScript[F, Seq[Base]] = ???
+    }
+
+    trait TblService[F[_]: Async] {
+
+      def createIfNotExists(base: Base, name: TblName): DbScript[F, Tbl] = {
+        for {
+          env <- ask[F, Env[F]]
+          // TODO: create segment and ix's
+          tblPath = Paths.get(f"${base.path.toAbsolutePath}/$name")
+          tbl = Tbl(name, tblPath)
+          _ <- env.files.createDirIfNotExists(tblPath)
+        } yield tbl
+      }
+
+      def list(base: Base): DbScript[F, Seq[Tbl]] = ???
+    }
+
+    trait TblSegmentService[F[_]] {
+      def create(tbl: Tbl, num: SegmentNum): DbScript[F, Segment] = ???
+
+      def append(s: Segment, batch: Seq[(Key, Value)]): DbScript[F, Segment] = ???
+
+      def readValue(s: Segment, offset: Offset): DbScript[F, Value] = ???
+    }
+
+    trait TblIxService[F[_]] {
+      def create(tbl: Tbl): DbScript[F, TblIx] = ???
+
+      def read(tbl: Tbl): DbScript[F, TblIx] = ???
+
+      def write(ix: TblIx): DbScript[F, TblIx] = ???
+    }
+
+    trait SegmentIxService[F[_]] {
+      def create(s: Segment): DbScript[F, SegmentIx] = ???
+
+      def read(tbl: Tbl, num: SegmentNum): DbScript[F, SegmentIx] = ???
+
+      def write(ix: SegmentIx): DbScript[F, SegmentIx] = ???
+    }
+
+    final case class Base(
+      name:   BaseName,
+      path:   Path,
+      tables: BaseTables = Map.empty)
+
+    final case class TblIx(
+      name: TblIxName,
+      path: Path,
+      data: TblIxData)
+
+    final case class SegmentIx(
+      name: String,
+      path: Path,
+      data: SegmentIxData)
+
+    final case class Tbl(
+      name:        TblName,
+      path:        Path,
+      lastSegment: Option[Segment] = None,
+      ix:          Option[TblIx] = None)
+
+    final case class Segment(
+      num:        SegmentNum,
+      name:       String,
+      path:       Path,
+      isReadOnly: Boolean,
+      offset:     Offset,
+      ix:         Option[SegmentIx] = None)
+
+    final case class State[F[_]](registryRef: Ref[F, BaseRegistry])
+
+    object RunDbScript {
+
+      def apply[F[_]: Async, O](s: DbScript[F, O])(using env: Env[F]): F[O] = {
+        s.run(env)
+      }
+    }
+  }
+}
