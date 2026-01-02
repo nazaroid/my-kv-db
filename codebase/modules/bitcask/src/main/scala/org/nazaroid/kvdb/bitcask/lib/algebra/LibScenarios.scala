7759775193b4ea503346637ce853f98f07933c45
artem.nazarenko <a.nazarenko@UZUM.COM>
2026-01-03 00:38:45 +0300

    • kvdb init in progress

--- codebase/modules/bitcask/src/main/scala/org/nazaroid/kvdb/bitcask/lib/algebra/LibScenarios.scala
+++ codebase/modules/bitcask/src/main/scala/org/nazaroid/kvdb/bitcask/lib/algebra/LibScenarios.scala
@@ -4,13 +4,68 @@ import cats.data.Kleisli
 import cats.data.Kleisli.ask
 import cats.effect.Async
 
+import java.nio.file.{Path, Paths}
+
 trait LibScenarios[F[_]: Async] {
   given env: Env[F]
 
   def readDbCatalog(): F[DbCatalog] = ???
 
   def init(dbCatalog: DbCatalog): F[Unit] = {
-    ???
+    val rootDir = "/Users/artem.nazarenko/IdeaProjects/my/my-kv-db/codebase/modules/server/kvdb/"
+    val baseName = "db"
+    val basePath = "/Users/artem.nazarenko/IdeaProjects/my/my-kv-db/codebase/modules/server/kvdb/db"
+
+    val tblName = "tbl"
+    val tblPath = "/Users/artem.nazarenko/IdeaProjects/my/my-kv-db/codebase/modules/server/kvdb/db/tbl"
+
+    val tblIxName = "table.ix"
+    val tblIxPath = "/Users/artem.nazarenko/IdeaProjects/my/my-kv-db/codebase/modules/server/kvdb/db/tbl/table.ix"
+
+    val segmentName = "segment_1.seg"
+    val segmentPath = "/Users/artem.nazarenko/IdeaProjects/my/my-kv-db/codebase/modules/server/kvdb/db/tbl/segment_1.seg"
+
+    val segmentIxName = "segment_1.ix.ix"
+    val segmentIxPath = "/Users/artem.nazarenko/IdeaProjects/my/my-kv-db/codebase/modules/server/kvdb/db/tbl/segment_1.ix"
+
+    def loadTblIxData(path: String): DbScript[F, TblIxData[F]] = ???
+
+    def loadSegmentIxData(path: String): DbScript[F, SegmentIxData] = ???
+
+    def loadSegment(path: String): DbScript[F, Segment[F]] = ???
+
+    DbScript.run {
+      for {
+        env  <- ask[F, Env[F]]
+        // TODO:
+        // - impl load funs
+        // - add to cache
+        
+        // plan: 
+        // create segmentIx
+        // create segment
+        // create tblIx
+        // create tbl
+        // create base
+
+        segmentIxData <- loadSegmentIxData(segmentIxPath)
+        segmentIxDataRef <- DbScript.lift(Async[F].ref(segmentIxData))
+        segmentIx = SegmentIx(segmentIxName, Paths.get(segmentIxPath), segmentIxDataRef)
+        segment <- loadSegment(segmentPath)
+        _ <- DbScript.lift(segment.ix.set(Some(segmentIx)))
+        tblIxData <- loadTblIxData(tblIxPath)
+        tblIxDataRef <- DbScript.lift(Async[F].ref(tblIxData))
+        tblIx = TblIx[F](tblIxName,  Paths.get(tblIxPath), tblIxDataRef)
+
+        tblIxRef <- DbScript.lift(Async[F].ref(Option(tblIx)))
+        lastSegmentRef <- DbScript.lift(Async[F].ref(Option(segment)))
+        tbl = Tbl[F](tblName, Paths.get(tblPath), lastSegmentRef, tblIxRef)
+        
+        baseTbls <- DbScript.lift(Async[F].ref(Map(tblName -> tbl)))
+        base = Base(baseName, Paths.get(basePath), baseTbls)
+        
+      } yield ()
+    }
   }
 
   def createBaseIfNotExists(baseName: String): F[Base[F]] =
