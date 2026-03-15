9dd28a5491acce3b9c71d873f90178170ea8aba5
artem.nazarenko <a.nazarenko@UZUM.COM>
2026-03-15 22:10:40 +0300

    • kvdb init in stats

--- codebase/modules/statistics/src/test/scala/org/nazaroid/kvdb/statistics/StatisticsServiceSpec.scala
+++ codebase/modules/statistics/src/test/scala/org/nazaroid/kvdb/statistics/StatisticsServiceSpec.scala
@@ -1,20 +1,19 @@
 package org.nazaroid.kvdb.statistics
 
 import cats.effect.IO
-import cats.effect.unsafe.implicits.global
 import fs2.concurrent.Channel
 import fs2.io.file.{Files, Path}
-import org.nazaroid.kvdb.bitcask.storage.{StorageConfig, StorageManager}
 import org.nazaroid.kvdb.binfileio.{FieldDef, FieldType, WriteTask}
+import org.nazaroid.kvdb.bitcask.storage.{StorageConfig, StorageManager}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
-import org.typelevel.log4cats.SelfAwareStructuredLogger
+import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 
-import java.nio.file.{Paths, Files as JFiles}
+import java.nio.file.Files as JFiles
 import scala.concurrent.duration.*
 
-class StatisticsServiceSpec extends AnyFunSuite with Matchers {
+sealed class StatisticsServiceSpec extends AnyFunSuite with Matchers {
 
   def withTempDirectory[T](test: Path => IO[T]): IO[T] = {
     IO.delay {
@@ -41,7 +40,7 @@ class StatisticsServiceSpec extends AnyFunSuite with Matchers {
         FieldDef("status", FieldType.RecordStatus),
         FieldDef("crc", FieldType.CRC32)
       )
-      
+
       val segmentSchema = List(
         FieldDef("keySize", FieldType.Int32),
         FieldDef("key", FieldType.StringUtf8(sizeFromField = "keySize")),
@@ -49,7 +48,7 @@ class StatisticsServiceSpec extends AnyFunSuite with Matchers {
         FieldDef("timestamp", FieldType.Timestamp),
         FieldDef("status", FieldType.RecordStatus)
       )
-      
+
       val tableSchema = List(
         FieldDef("keySize", FieldType.Int32),
         FieldDef("key", FieldType.StringUtf8(sizeFromField = "keySize")),
@@ -58,43 +57,44 @@ class StatisticsServiceSpec extends AnyFunSuite with Matchers {
         FieldDef("timestamp", FieldType.Timestamp),
         FieldDef("status", FieldType.RecordStatus)
       )
-      
+
       val storageConfig = StorageConfig(
-        folder = tempDir.toString,
-        maxSegmentSize = 1024,
+        folder          = tempDir.toString,
+        maxSegmentSize  = 1024,
         maxSegmentCount = 10,
-        dataSchema = dataSchema,
-        segmentSchema = segmentSchema,
-        tableSchema = tableSchema,
-        maxRetries = 3
+        dataSchema      = dataSchema,
+        segmentSchema   = segmentSchema,
+        tableSchema     = tableSchema,
+        maxRetries      = 3
       )
-      
+
       val monitoringConfig = MonitoringConfig(
-        checkInterval = 1.second,
+        checkInterval              = 1.second,
         enableBackgroundMonitoring = false, // Disable for test
-        maxStaleRatio = 0.3,
-        compactionThreshold = 0.5
+        maxStaleRatio              = 0.3,
+        compactionThreshold        = 0.5
       )
       for {
-        queue <- Channel.unbounded[IO, WriteTask[IO]]
-        storageManager <- StorageManager.initialize(storageConfig, queue)
+        given Logger[IO]  <- Slf4jLogger.create[IO]
+        queue             <- Channel.unbounded[IO, WriteTask[IO]]
+        storageManager    <- StorageManager.initialize(storageConfig, queue)
         statisticsService <- StatisticsService.create(storageManager, monitoringConfig)
-        
+
         // Add test data
         _ <- storageManager.write("test_table/key1", "value1")
         _ <- storageManager.write("test_table/key2", "value2")
         _ <- storageManager.write("test_table/key3", "value3")
         _ <- storageManager.delete("test_table/key3") // Delete one entry
-        
+
         // Collect statistics
         result <- statisticsService.getDatabases
-        
+
       } yield {
         result should have size 1
         val dbInfo = result.head
         dbInfo.name should include("test")
-        dbInfo.totalEntries should be(3) // 3 entries total
-        dbInfo.activeEntries should be(2) // 2 active (one deleted)
+        dbInfo.totalEntries should be(3)   // 3 entries total
+        dbInfo.activeEntries should be(2)  // 2 active (one deleted)
         dbInfo.deletedEntries should be(1) // 1 deleted
         dbInfo.totalDiskSize should be > 0L
         dbInfo.totalMemorySize should be > 0L
@@ -111,89 +111,6 @@ class StatisticsServiceSpec extends AnyFunSuite with Matchers {
     }
   }
 
-  test("StatisticsService should export Prometheus format") {
-    withTempDirectory { tempDir =>
-      val dataSchema = List(
-        FieldDef("recordSize", FieldType.Int32),
-        FieldDef("value", FieldType.StringUtf8(sizeFromField = "recordSize")),
-        FieldDef("timestamp", FieldType.Timestamp),
-        FieldDef("status", FieldType.RecordStatus),
-        FieldDef("crc", FieldType.CRC32)
-      )
-      
-      val segmentSchema = List(
-        FieldDef("keySize", FieldType.Int32),
-        FieldDef("key", FieldType.StringUtf8(sizeFromField = "keySize")),
-        FieldDef("offset", FieldType.Int64),
-        FieldDef("timestamp", FieldType.Timestamp),
-        FieldDef("status", FieldType.RecordStatus)
-      )
-      
-      val tableSchema = List(
-        FieldDef("keySize", FieldType.Int32),
-        FieldDef("key", FieldType.StringUtf8(sizeFromField = "keySize")),
-        FieldDef("segmentNameSize", FieldType.Int32),
-        FieldDef("segmentName", FieldType.StringUtf8(sizeFromField = "segmentNameSize")),
-        FieldDef("timestamp", FieldType.Timestamp),
-        FieldDef("status", FieldType.RecordStatus)
-      )
-      
-      val storageConfig = StorageConfig(
-        folder = tempDir.toString,
-        maxSegmentSize = 1024,
-        maxSegmentCount = 10,
-        dataSchema = dataSchema,
-        segmentSchema = segmentSchema,
-        tableSchema = tableSchema,
-        maxRetries = 3
-      )
-      
-      val monitoringConfig = MonitoringConfig(
-        enableBackgroundMonitoring = false
-      )
-      
-      for {
-        queue <- Channel.unbounded[IO, WriteTask[IO]]
-        storageManager <- StorageManager.initialize(storageConfig, queue)
-        statisticsService <- StatisticsService.create(storageManager, monitoringConfig)
-        
-        // Add test data
-        _ <- storageManager.write("test_table/key1", "value1")
-        _ <- storageManager.write("test_table/key2", "value2")
-        
-        // Export Prometheus format
-        result <- statisticsService.exportForPrometheus()
-        
-      } yield {
-        // Check Prometheus format
-        result should include("# HELP kvdb_database_info")
-        result should include("# TYPE kvdb_database_info gauge")
-        result should include("kvdb_database_info{database=")
-        result should include("type=\"total_entries\"}")
-        result should include("type=\"active_entries\"}")
-        result should include("type=\"deleted_entries\"}")
-        result should include("type=\"disk_size_bytes\"}")
-        result should include("type=\"memory_size_bytes\"}")
-        result should include("type=\"fragmentation_ratio\"}")
-
-        result should include("# HELP kvdb_table_info")
-        result should include("# TYPE kvdb_table_info gauge")
-        result should include("kvdb_table_info{database=")
-        result should include("table=\"test_table\"}")
-
-        result should include("# HELP kvdb_segment_info")
-        result should include("# TYPE kvdb_segment_info gauge")
-        result should include("kvdb_segment_info{database=")
-        result should include("segment=")
-        result should include("type=\"file_size_bytes\"}")
-        result should include("type=\"is_active\"}")
-        result should include("type=\"stale_ratio\"}")
-        result should include("type=\"entry_count\"}")
-      }
-      
-    }
-  }
-
   test("StatisticsIntegration should provide health checks") {
     withTempDirectory { tempDir =>
       val dataSchema = List(
@@ -203,7 +120,7 @@ class StatisticsServiceSpec extends AnyFunSuite with Matchers {
         FieldDef("status", FieldType.RecordStatus),
         FieldDef("crc", FieldType.CRC32)
       )
-      
+
       val segmentSchema = List(
         FieldDef("keySize", FieldType.Int32),
         FieldDef("key", FieldType.StringUtf8(sizeFromField = "keySize")),
@@ -211,7 +128,7 @@ class StatisticsServiceSpec extends AnyFunSuite with Matchers {
         FieldDef("timestamp", FieldType.Timestamp),
         FieldDef("status", FieldType.RecordStatus)
       )
-      
+
       val tableSchema = List(
         FieldDef("keySize", FieldType.Int32),
         FieldDef("key", FieldType.StringUtf8(sizeFromField = "keySize")),
@@ -220,33 +137,34 @@ class StatisticsServiceSpec extends AnyFunSuite with Matchers {
         FieldDef("timestamp", FieldType.Timestamp),
         FieldDef("status", FieldType.RecordStatus)
       )
-      
+
       val storageConfig = StorageConfig(
-        folder = tempDir.toString,
-        maxSegmentSize = 1024,
+        folder          = tempDir.toString,
+        maxSegmentSize  = 1024,
         maxSegmentCount = 10,
-        dataSchema = dataSchema,
-        segmentSchema = segmentSchema,
-        tableSchema = tableSchema,
-        maxRetries = 3
+        dataSchema      = dataSchema,
+        segmentSchema   = segmentSchema,
+        tableSchema     = tableSchema,
+        maxRetries      = 3
       )
-      
+
       val monitoringConfig = MonitoringConfig(
         enableBackgroundMonitoring = false
       )
-      
+
       for {
-        queue <- Channel.unbounded[IO, WriteTask[IO]]
-        storageManager <- StorageManager.initialize(storageConfig, queue)
+        given Logger[IO]      <- Slf4jLogger.create[IO]
+        queue                 <- Channel.unbounded[IO, WriteTask[IO]]
+        storageManager        <- StorageManager.initialize(storageConfig, queue)
         statisticsIntegration <- StatisticsIntegration.create(storageManager, monitoringConfig)
-        
+
         // Add test data
         _ <- storageManager.write("test_table/key1", "value1")
         _ <- storageManager.write("test_table/key2", "value2")
-        
+
         // Get health check
         result <- statisticsIntegration.getHealthCheck
-        
+
       } yield {
         result.totalDatabases should be(1)
         result.healthyDatabases should be(1)
@@ -259,15 +177,15 @@ class StatisticsServiceSpec extends AnyFunSuite with Matchers {
 
   test("SegmentInfo should calculate correct metrics") {
     val segmentInfo = SegmentInfo(
-      name = "seg_12345",
-      filePath = "/test/seg_12345.bin",
-      fileSize = 1024L,
-      isActive = true,
+      name           = "seg_12345",
+      filePath       = "/test/seg_12345.bin",
+      fileSize       = 1024L,
+      isActive       = true,
       staleDataRatio = 0.25,
-      entryCount = 10,
-      lastModified = System.currentTimeMillis()
+      entryCount     = 10,
+      lastModified   = System.currentTimeMillis()
     )
-    
+
     segmentInfo.name should be("seg_12345")
     segmentInfo.fileSize should be(1024L)
     segmentInfo.isActive should be(true)
@@ -281,18 +199,18 @@ class StatisticsServiceSpec extends AnyFunSuite with Matchers {
       TableInfo("users", 100, 95, 10240L, 5000L),
       TableInfo("orders", 200, 190, 20480L, 10000L)
     )
-    
+
     val dbInfo = DatabaseInfo(
-      name = "test_db",
-      tables = tables,
-      totalEntries = 300,
-      activeEntries = 285,
-      deletedEntries = 15,
-      totalDiskSize = 30720L,
-      totalMemorySize = 15000L,
+      name               = "test_db",
+      tables             = tables,
+      totalEntries       = 300,
+      activeEntries      = 285,
+      deletedEntries     = 15,
+      totalDiskSize      = 30720L,
+      totalMemorySize    = 15000L,
       fragmentationRatio = 0.15
     )
-    
+
     dbInfo.name should be("test_db")
     dbInfo.tables should have size 2
     dbInfo.totalEntries should be(300)
@@ -301,7 +219,7 @@ class StatisticsServiceSpec extends AnyFunSuite with Matchers {
     dbInfo.totalDiskSize should be(30720L)
     dbInfo.totalMemorySize should be(15000L)
     dbInfo.fragmentationRatio should be(0.15)
-    
+
     // Check table aggregation
     dbInfo.tables.map(_.entryCount).sum should be(300)
     dbInfo.tables.map(_.activeEntryCount).sum should be(285)
@@ -309,7 +227,7 @@ class StatisticsServiceSpec extends AnyFunSuite with Matchers {
 
   test("MonitoringConfig should have sensible defaults") {
     val config = MonitoringConfig()
-    
+
     config.checkInterval should be(30.seconds)
     config.enableBackgroundMonitoring should be(true)
     config.maxStaleRatio should be(0.3)
@@ -318,29 +236,29 @@ class StatisticsServiceSpec extends AnyFunSuite with Matchers {
 
   test("HealthStatus should calculate correct status") {
     val healthyStatus = HealthStatus(
-      status = HealthStatus.Healthy,
-      totalDatabases = 2,
-      healthyDatabases = 2,
+      status               = HealthStatus.Healthy,
+      totalDatabases       = 2,
+      healthyDatabases     = 2,
       averageFragmentation = 0.2,
-      timestamp = System.currentTimeMillis()
+      timestamp            = System.currentTimeMillis()
     )
-    
+
     val degradedStatus = HealthStatus(
-      status = HealthStatus.Degraded,
-      totalDatabases = 2,
-      healthyDatabases = 1,
+      status               = HealthStatus.Degraded,
+      totalDatabases       = 2,
+      healthyDatabases     = 1,
       averageFragmentation = 0.4,
-      timestamp = System.currentTimeMillis()
+      timestamp            = System.currentTimeMillis()
     )
-    
+
     val unhealthyStatus = HealthStatus(
-      status = HealthStatus.Unhealthy,
-      totalDatabases = 2,
-      healthyDatabases = 0,
+      status               = HealthStatus.Unhealthy,
+      totalDatabases       = 2,
+      healthyDatabases     = 0,
       averageFragmentation = 0.7,
-      timestamp = System.currentTimeMillis()
+      timestamp            = System.currentTimeMillis()
     )
-    
+
     healthyStatus.status should be(HealthStatus.Healthy)
     degradedStatus.status should be(HealthStatus.Degraded)
     unhealthyStatus.status should be(HealthStatus.Unhealthy)
