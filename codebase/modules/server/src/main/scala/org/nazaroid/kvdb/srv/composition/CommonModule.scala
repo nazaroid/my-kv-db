5dc2adde6238688f9278581f6482bacc63491563
artem.nazarenko <a.nazarenko@UZUM.COM>
2026-05-12 21:15:14 +0300

    • stat: in progress

--- codebase/modules/server/src/main/scala/org/nazaroid/kvdb/srv/composition/CommonModule.scala
+++ codebase/modules/server/src/main/scala/org/nazaroid/kvdb/srv/composition/CommonModule.scala
@@ -8,7 +8,7 @@ import org.nazaroid.kvdb.srv.DbInstanceConfig
 final class CommonModule[F[_]: Async](
   c: DbInstanceConfig,
   d: Dispatcher[F]) {
-  implicit val config:     DbInstanceConfig  = c
   implicit val dispatcher: Dispatcher[F]     = d
+  val config:     DbInstanceConfig  = c
   val metricRegistry:   CollectorRegistry = CollectorRegistry.defaultRegistry
 }
